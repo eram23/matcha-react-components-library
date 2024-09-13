459ba3e7842eb8171ac40899f2ce5314a13fa54c
Erik <erikramirezatl@gmail.com>
2024-09-12 20:54:57 -0400

    added a dark and light theme toggle btn

--- components/NavMenu/NavMenu.jsx
+++ components/NavMenu/NavMenu.jsx
@@ -2,26 +2,23 @@ import React from 'react'
 import useToggle from './useToggle'
 import NavDropDown from './NavDropDown'
 import NavItem from './NavItem'
+import { ThemeContext } from '/index.jsx'
 
 
 
 export default function NavMenu({children}) {
     const [open, setOpen] = React.useState(false)
+    const theme = React.useContext(ThemeContext)
 
     function toggleOpen(e) {
         setOpen(prevValue => !prevValue)
-
-        // if(e.target.className === "fa-solid fa-bars") {
-        //     setOpen(prevValue => !prevValue)
-        // } else if (e.target.className === "dropdown-nav" ) {
-        //     setOpen(prevValue => !prevValue)
-
-        // }
-        console.log(e.target)
     }
 
+    const matchaNavTheme = theme === 'dark' ? 'matcha-nav-dark' : 'matcha-nav'
+    const dropdownNavTheme = 'dropdown-nav-' + theme
+
     return (
-            <nav className="matcha-nav">
+            <nav className={matchaNavTheme}>
                 
                 <span className="logo">
                     <img src="./images/logo-ex-2.svg"/>
@@ -31,7 +28,7 @@ export default function NavMenu({children}) {
                 <a href="#" onClick={toggleOpen}><i className="fa-solid fa-bars"></i></a>
                 {
                     open ? 
-                    <ul className="dropdown-nav">
+                    <ul className={dropdownNavTheme}>
                         <a href="#" onClick={toggleOpen}><i className="fa-solid fa-xmark"></i></a>
                         <ul>
                             <li>home</li>
--- index.jsx
+++ index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'  
 import ReactDOM from 'react-dom/client';
 import Button from "./components/Button/Button"
 import Card from "./components/Cards/index"
@@ -8,84 +8,99 @@ import NavItem from './components/NavMenu/NavItem';
 import NavDropDown from './components/NavMenu/NavDropDown';
 import Banner from './components/Banner/index'
 import Footer from './components/Footer/index'  
+import classNames from 'classnames'
+
+const ThemeContext = React.createContext('light')
 
 function App() {
+  const [theme, setTheme] = React.useState('dark')
+
+  function toggleTheme() {
+    setTheme(theme === 'dark'? 'light' : 'dark')
+    console.log(theme)
+  }
+  const headerClass = theme === 'dark' ? 'header-dark' : null
+
+  // allClasses = classNames()
   return (
-    <>
+    <ThemeContext.Provider value={theme}>
     
-      <main>
-        <h1>Matcha Component Library 🍵</h1>
-        <p>React component library for all the matcha lovers who want to add some 
-        earthy flavor to their project. This library consists of buttons, cards, banners,
-        and testimonials.
-        </p>
-        
-        <h2>Matcha Buttons</h2>
-        <div className="component-container btn-container">
-          <Button className="matcha-btn">Primary Button</Button>
-          <Button className="matcha-btn secondary">Secondary Button</Button>
-          <Button className="matcha-btn disabled">Disabled Button</Button>
-        </div>
-
-        <h2>Matcha Nav</h2>
-        <div className="component-container">
-          <NavMenu>
-            <NavDropDown>
-              <NavItem>home</NavItem>
-              <NavItem>about</NavItem>
-            </NavDropDown>
-          </NavMenu>
-        </div>
-        
-        <h2>Matcha Card</h2>
-        <div className="component-container">
-          <Card>
-            <Card.Title>Easy Deployment</Card.Title>
-            <Card.Body>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-            </Card.Body>
-          </Card>
-        </div>
-        
-        <h2>Matcha Testimonial</h2>
-        <div className="component-container">
-          <Testimonial>
-            <Testimonial.Logo >
-              <img src="/images/logo-ex.png" />
-            </Testimonial.Logo>
-            <Testimonial.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.
-            </Testimonial.Body>
-            <Testimonial.Source><span className="testimonial-author">May Andersons / </span>Workcation, CTO</Testimonial.Source>
-          </Testimonial>
-        </div>
-
-        <h2>Matcha Banners</h2>
-        <div className="component-container">
-          <Banner className="banner">
-            <img src="./images/success.svg" />
-            <Banner.Content>
-              <Banner.Title>Congratulations</Banner.Title>
-              <Banner.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Body>
-            </Banner.Content>
-          </Banner>
+      <main className={theme}>
+        <div className="container">
+          <h1 className={headerClass}>Matcha Component Library 🍵</h1>
+          <button className="toggle-theme-btn" onClick={toggleTheme}>Light</button>
+          <p className={theme}>React component library for all the matcha lovers who want to add some 
+          earthy flavor to their project. This library consists of buttons, cards, banners,
+          and testimonials.
+          </p>
           
-          <Banner className="banner neutral">
-            <img src="./images/warning.svg" />
-            <Banner.Content>
-              <Banner.Title>Attention</Banner.Title>
-              <Banner.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Body>
-            </Banner.Content>
-          </Banner>
-          
-          <Banner className="banner neutral">
-            <img src="./images/neutral.svg" />
-            <Banner.Content>
-              <Banner.Title>Update Available</Banner.Title>
-              <Banner.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Body>
-            </Banner.Content>
-          </Banner>
-        </div> 
+          <h2 className={headerClass}>Matcha Buttons</h2>
+          <div className="component-container btn-container">
+            <Button className="matcha-btn">Primary Button</Button>
+            <Button className="matcha-btn secondary">Secondary Button</Button>
+            <Button className="matcha-btn disabled">Disabled Button</Button>
+          </div>
 
-        <h2>Matcha Footer</h2>
+          <h2 className={headerClass}>Matcha Nav</h2>
+          <div className="component-container">
+            <NavMenu>
+              <NavDropDown>
+                <NavItem>home</NavItem>
+                <NavItem>about</NavItem>
+              </NavDropDown>
+            </NavMenu>
+          </div>
+          
+          <h2 className={headerClass}>Matcha Card</h2>
+          <div className="component-container">
+            <Card>
+              <Card.Title>Easy Deployment</Card.Title>
+              <Card.Body>Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
+              </Card.Body>
+            </Card>
+          </div>
+          
+          <h2 className={headerClass}>Matcha Testimonial</h2>
+          <div className="component-container">
+            <Testimonial>
+              <Testimonial.Logo >
+                <img src="/images/logo-ex.png" />
+              </Testimonial.Logo>
+              <Testimonial.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.
+              </Testimonial.Body>
+              <Testimonial.Source><span className="testimonial-author">May Andersons / </span>Workcation, CTO</Testimonial.Source>
+            </Testimonial>
+          </div>
+
+          <h2 className={headerClass}>Matcha Banners</h2>
+          <div className="component-container">
+            <Banner className="banner">
+              <img src="./images/success.svg" />
+              <Banner.Content>
+                <Banner.Title>Congratulations</Banner.Title>
+                <Banner.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Body>
+              </Banner.Content>
+            </Banner>
+            
+            <Banner className="banner neutral">
+              <img src="./images/warning.svg" />
+              <Banner.Content>
+                <Banner.Title>Attention</Banner.Title>
+                <Banner.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Body>
+              </Banner.Content>
+            </Banner>
+            
+            <Banner className="banner neutral">
+              <img src="./images/neutral.svg" />
+              <Banner.Content>
+                <Banner.Title>Update Available</Banner.Title>
+                <Banner.Body>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner.Body>
+              </Banner.Content>
+            </Banner>
+          </div> 
+
+          <h2 className={headerClass}>Matcha Footer</h2>
+        </div>
       </main>
         
         <Footer>
@@ -104,8 +119,10 @@ function App() {
             </Footer.Pages>
           </Footer.Content>
         </Footer>
-    </>
+    </ThemeContext.Provider>
   )
 }
+
+export { ThemeContext }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
